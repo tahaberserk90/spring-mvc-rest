32f955b930e0aa633b8dba45d26164faa87052c1
tahaberserk90 <tahaberserk@gmail.com>
2020-03-12 23:13:21 +0100

    start project

---
+++ src/main/java/com/rest/api/spring/SpringMvcRestApplication.java
@@ -0,0 +1,13 @@
+package com.rest.api.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcRestApplication.class, args);
+	}
+
+}
